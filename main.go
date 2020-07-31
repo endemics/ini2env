ac24604e61774e7e854f6fcf451131c6ffdea2e9
Gildas Le Nadan <gildas@endemic-systems.com>
2020-07-31 18:49:57 +1000

    Add option -booleans to allow conversion of "boolean-looking" values to 1 or 0 Conversion will be according to: - 1, t, T, TRUE, true, True, YES, yes, Yes, y, ON, on, On - 0, f, F, FALSE, false, False, NO, no, No, n, OFF, off, Off

--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 )
 
 var (
-	prefix string
-	file   string
+	prefix       string
+	file         string
+	bool_convert bool
 )
 
 func main() {
 	flag.StringVar(&prefix, "prefix", "INI", "the prefix to add to the variable names")
 	flag.StringVar(&file, "file", "file.ini", "the ini file to parse")
+	flag.BoolVar(&bool_convert, "booleans", false, "recognise special boolean values and convert them to 1 (true) or 0 (false)")
 	flag.Parse()
 
 	cfg, err := ini.Load(file)
@@ -27,11 +29,22 @@ func main() {
 	for _, section := range sections {
 		keys := cfg.Section(section).KeyStrings()
 		for _, key := range keys {
+			value := cfg.Section(section).Key(key).String()
+
+			b, err := cfg.Section(section).Key(key).Bool()
+			if bool_convert && err == nil {
+				if b {
+					value = "1"
+				} else {
+					value = "0"
+				}
+			}
+
 			fmt.Printf("%s__%s__%s=\"%s\"\n",
 				prefix,
 				section,
 				key,
-				cfg.Section(section).Key(key).String(),
+				value,
 			)
 		}
 	}
